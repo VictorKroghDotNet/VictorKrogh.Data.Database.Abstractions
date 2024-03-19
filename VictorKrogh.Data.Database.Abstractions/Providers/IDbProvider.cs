5810f106826aff8f3b7b5d8c438cfd4d69d6748d
KingoBoiii <kingodk@outlook.dk>
2024-03-19 17:59:52 +0100

    Using ValueTask instead of Task

--- VictorKrogh.Data.Database.Abstractions/Providers/IDbProvider.cs
+++ VictorKrogh.Data.Database.Abstractions/Providers/IDbProvider.cs
@@ -8,42 +8,42 @@ public interface IDbProvider : IProvider
 {
     string GetQualifiedTableName<TModel>() where TModel : Model;
 
-    Task<TModel?> GetAsync<TModel, TKey>(TKey key, int? commandTimeout = null)
+    ValueTask<TModel?> GetAsync<TModel, TKey>(TKey key, int? commandTimeout = null)
         where TModel : Model
         where TKey : notnull;
 
-    Task<IEnumerable<TModel?>> GetAllAsync<TModel>(int? commandTimeout = null)
+    ValueTask<IEnumerable<TModel?>> GetAllAsync<TModel>(int? commandTimeout = null)
         where TModel : Model;
 
-    Task<bool> InsertAsync<TModel>(TModel model, int? commandTimeout = null)
+    ValueTask<bool> InsertAsync<TModel>(TModel model, int? commandTimeout = null)
         where TModel : Model;
 
-    Task<bool> UpdateAsync<TModel>(TModel model, int? commandTimeout = null)
+    ValueTask<bool> UpdateAsync<TModel>(TModel model, int? commandTimeout = null)
         where TModel : Model;
 
-    Task<bool> DeleteAsync<TModel>(TModel model, int? commandTimeout = null)
+    ValueTask<bool> DeleteAsync<TModel>(TModel model, int? commandTimeout = null)
         where TModel : Model;
 
-    Task<bool> DeleteAllAsync<TModel>(int? commandTimeout = null)
+    ValueTask<bool> DeleteAllAsync<TModel>(int? commandTimeout = null)
         where TModel : Model;
 
-    Task<TModel> QuerySingleAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    ValueTask<TModel> QuerySingleAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
         where TModel : Model;
 
-    Task<TModel?> QuerySingleOrDefaultAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    ValueTask<TModel?> QuerySingleOrDefaultAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
         where TModel : Model;
 
-    Task<TModel> QueryFirstAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    ValueTask<TModel> QueryFirstAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
         where TModel : Model;
 
-    Task<TModel?> QueryFirstOrDefaultAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    ValueTask<TModel?> QueryFirstOrDefaultAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
         where TModel : Model;
 
-    Task<IEnumerable<TModel?>> QueryAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
+    ValueTask<IEnumerable<TModel?>> QueryAsync<TModel>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null)
         where TModel : Model;
 
-    Task<int> ExecuteAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
-    Task<T?> ExecuteScalarAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
-    Task<T?> ExecuteSingleOrDefaultAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
-    Task<IEnumerable<T?>> ExecuteQueryAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
+    ValueTask<int> ExecuteAsync(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
+    ValueTask<T?> ExecuteScalarAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
+    ValueTask<T?> ExecuteSingleOrDefaultAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
+    ValueTask<IEnumerable<T?>> ExecuteQueryAsync<T>(string sql, object? parameters = null, int? commandTimeout = null, CommandType? commandType = null);
 }
